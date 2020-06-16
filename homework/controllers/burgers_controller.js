920ea299a2606fac66c798c37cf80b06e4747f83
Celeste J Bembry <CelesteJBembry@gmail.com>
2020-06-16 10:12:22 -0500

    minor updates to orm

--- homework/controllers/burgers_controller.js
+++ homework/controllers/burgers_controller.js
@@ -8,7 +8,7 @@ var burger = require("../models/burger.js");
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
-      burgers: data
+      burger_name: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -43,18 +43,6 @@ router.post("/api/burgers", function(req, res) {
 //     }
 //   });
 // });
-
-// router.delete("/api/burgers/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   burger.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
 // });
 
 // Export routes for server.js to use.
